bb2f9d5bf5ea6a0bd7ccf6ccaaa6a1bb8a9c371a
Navjot Singh <navjotd139@gmail.com>
2019-06-21 16:46:35 +0530

    toast and seekbar code implemented

--- app/src/main/java/com/example/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/example/notificationscheduler/MainActivity.java
@@ -1,13 +1,112 @@
 package com.example.notificationscheduler;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private JobScheduler mScheduler;
+    private static final int JOB_ID = 0;
+    private Switch mDeviceIdleSwitch;
+    private Switch mDeviceChargingSwitch;
+    private SeekBar mSeekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDeviceIdleSwitch = findViewById(R.id.idleSwitch);
+        mDeviceChargingSwitch = findViewById(R.id.chargeSwitch);
+        mSeekbar = findViewById(R.id.seekbar);
+        final TextView seekbarProgress = findViewById(R.id.seekbarProgress);
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(progress>0){
+                    seekbarProgress.setText(progress +" s");
+                }
+                else{
+                    seekbarProgress.setText("Not Set");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+
+    }
+
+    public void ScheduleJob(View view) {
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        RadioGroup networkOptions = findViewById(R.id.options);
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        int seekBarInteger = mSeekbar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
+
+        switch (selectedNetworkID){
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+        }
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
+         .setRequiredNetworkType(selectedNetworkOption)
+                .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked())
+                .setRequiresCharging(mDeviceChargingSwitch.isChecked());
+
+        if(seekBarSet){
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
+
+        boolean constraintSet = (selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE)
+                || mDeviceChargingSwitch.isChecked() || mDeviceIdleSwitch.isChecked() || seekBarSet;
+
+        if(constraintSet) {
+            JobInfo info = builder.build();
+            mScheduler.schedule(info);
+
+            Toast.makeText(this, "Job Scheduled, job will run when " +
+                    "the constraints are met.", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Please set at least one constraint",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void CancelJob(View view) {
+
+        if(mScheduler != null){
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
